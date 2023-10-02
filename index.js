58c7cd6a8ecacdd9efe59a5a8c76a34dfe9171f2
Navashub <navasmuller01@gmail.com>
2023-10-02 15:38:44 +0300

    task done

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+// function submitData(name, email){
+//     const data = {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             Accept: "application/json",
+//         },
+//         body:JSON.stringify({
+//             name: 'Name',
+//             email: 'Email'
+//         }),
+//     };
+//     fetch("http://localhost:3000/users", submitData)
+// }
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                document.querySelector('body').innerHTML = data.id
+            })
+            .catch(error => {
+                document.querySelector('body').innerHTML = error.message
+            })
+
+}
